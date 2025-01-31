26d519677126ba3086354566e4366e856199f831
“Michael <michaelprattcoding@gmail.comgit config --global core.editor code>
2025-01-30 19:29:50 -0800

    new changes

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const db = require("./config/connection");
+const routes = require("./routes");
 
-const app = express();
+const cwd = process.cwd();
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.json());
+app.use(routes);
 
-app.use(require("./routes"));
-
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/social-network-API",
-  {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-// Use this to log mongo queries being executed!
-mongoose.set("debug", true);
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(
+      `API server for social network challenge running on port ${PORT}!`
+    );
+  });
+});
